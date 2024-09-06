4ad095aa4e198f3c5093a182ff29f70c6046b310
MiaSibuea <miasibuea152@gmail.com>
2024-09-06 16:03:36 +0700

    pair 14

--- T01.java
+++ T01.java
@@ -1,1 +1,26 @@
-// NIM - Your Name
+// 12S24005 - MIA NATHANIA SIBUEA
+// 12S24036 - JOSEF CHRISTIAN MARPAUNG
+import java.util.*;
+import java.lang.Math;
+
+public class T01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penulis, penerbit, formatbuku;
+        int tahunterbit, stok;
+        double hargapembelian, minimummargin, rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunterbit = Integer.parseInt(input.nextLine());
+        penerbit = input.nextLine();
+        formatbuku = input.nextLine();
+        hargapembelian = Double.parseDouble(input.nextLine());
+        minimummargin = Double.parseDouble(input.nextLine());
+        stok = Integer.parseInt(input.nextLine());
+        rating = Double.parseDouble(input.nextLine());
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunterbit + "|" + penerbit + "|" + formatbuku + "|" + hargapembelian + "|" + minimummargin + "|" + stok + "|" + rating);
+    }
+}
